7dd2a318e994cf806c478a24ccade7a856da2408
jopakka <jopakkamies@gmail.com>
2022-12-05 19:39:23 +0200

    updated ui

--- app/src/main/java/fi/joonasniemi/movierandomizer/ui/movie/MovieScreen.kt
+++ app/src/main/java/fi/joonasniemi/movierandomizer/ui/movie/MovieScreen.kt
@@ -1,17 +1,19 @@
 package fi.joonasniemi.movierandomizer.ui.movie
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -23,10 +25,18 @@ fun MovieScreen(
     val loading by movieViewModel.loading.collectAsState()
     val movie by movieViewModel.movie.collectAsState()
 
-    Scaffold(modifier = Modifier.fillMaxWidth()) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)) {
             if(loading) {
-                CircularProgressIndicator()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator(1f)
+                }
                 return@Column
             }
             movie?.let {
